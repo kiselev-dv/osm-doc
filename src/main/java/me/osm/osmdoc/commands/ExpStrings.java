a281eea84e089aaa19f7d78bad05732a7c21c31e
kiselev-dv <dmitry.v.kiselev@gmail.com>
2014-10-01 16:12:03 +0200

    Output NS mapper

--- src/main/java/me/osm/osmdoc/commands/ExpStrings.java
+++ src/main/java/me/osm/osmdoc/commands/ExpStrings.java
@@ -15,17 +15,23 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
+import me.osm.osmdoc.imports.osmcatalog.DocPartNSMapper;
 import me.osm.osmdoc.model.DocPart;
 import me.osm.osmdoc.model.Feature;
+import me.osm.osmdoc.model.Group;
+import me.osm.osmdoc.model.Hierarchy;
 import me.osm.osmdoc.model.KeyType;
 import me.osm.osmdoc.model.MoreTags;
 import me.osm.osmdoc.model.Tag;
 import me.osm.osmdoc.model.Tag.Val;
+import me.osm.osmdoc.model.Tags;
 
 import org.apache.commons.lang3.StringUtils;
 
 public class ExpStrings {
 
+	private static final DocPartNSMapper DOC_PART_NS_MAPPER = new DocPartNSMapper();
+	
 	private String catalogPath;
 	private Properties properties;
 
@@ -78,6 +84,7 @@ public class ExpStrings {
 			
 			Marshaller marshaller = jaxbContext.createMarshaller();
 			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+			marshaller.setProperty("com.sun.xml.bind.namespacePrefixMapper", DOC_PART_NS_MAPPER);
 			marshaller.marshal(doc, root);
 			
 		}
@@ -87,65 +94,114 @@ public class ExpStrings {
 	}
 
 	private void process(DocPart doc) {
+		
 		for(Feature f : doc.getFeature()) {
-			String name = f.getName();
-			
-			if(!f.getTitle().startsWith(L10N_PREFIX)) {
-				String i18nKey = L10N_PREFIX + name + ".title";
-				properties.setProperty(i18nKey, f.getTitle());
-				f.setTitle(i18nKey);
-			}
-			
-			if(StringUtils.stripToNull(f.getDescription()) != null 
-					&& !f.getDescription().startsWith(L10N_PREFIX)) {
-				
-				String i18nKey = L10N_PREFIX + name + ".description";
-				properties.setProperty(i18nKey, f.getDescription());
-				f.setDescription(i18nKey);
-				
-			}
-			
-			List<MoreTags> moreTags = f.getMoreTags();
-			for(MoreTags mt : moreTags) {
-				for(Tag tag : mt.getTag()) {
-					KeyType key = tag.getKey();
-					
-					String title = tag.getTitle();
-					if(!title.startsWith(L10N_PREFIX)) {
-						String i18nKey = L10N_PREFIX + name + ".moreTags." + key.getValue() + ".title";
-						properties.setProperty(i18nKey, title);
-						
-						tag.setTitle(i18nKey);
-					}
-					
-					String description = tag.getDescription();
-					if(description!= null && !description.startsWith(L10N_PREFIX)) {
-						String i18nKey = L10N_PREFIX + name + ".moreTags."+ key.getValue() + ".description";
-						properties.setProperty(i18nKey, description);
-						
-						tag.setTitle(i18nKey);
-					}
-					
-					for(Val val : tag.getVal()) {
-						String valueKey = val.getValue();
-						String valTitle = val.getTitle();
-						String valDesc = val.getDescription();
-						
-						if(!valTitle.startsWith(L10N_PREFIX)) {
-							String i18nKey = L10N_PREFIX + name + ".moreTags."+ key.getValue() + "." + valueKey + ".title";
-							properties.setProperty(i18nKey, valTitle);
-							val.setTitle(i18nKey);
-						}
+			processFeature(f);
+		}
+		
+		for (Hierarchy h : doc.getHierarchy()) {
+			processHierarchy(h);
+		}
+	}
 
-						if(valDesc != null && !valDesc.startsWith(L10N_PREFIX)) {
-							String i18nKey = L10N_PREFIX + name + ".moreTags."+ key.getValue() + "." + valueKey + ".description";
-							properties.setProperty(i18nKey, valDesc);
-							val.setDescription(i18nKey);
-						}
-					}
-				}
-			}
+	private void processHierarchy(Hierarchy h) {
+		for(Group g : h.getGroup()) {
 			
+			String gTitle = g.getTitle();
+			
+			if(!gTitle.startsWith(L10N_PREFIX)) {
+				String i18nKey = L10N_PREFIX + "group." + h.getName() + "." + g.getName() + ".title";
+				properties.setProperty(i18nKey, gTitle);
+				g.setTitle(i18nKey);
+			}
+
+			String gDesc = g.getDescription();
+			
+			if(gDesc != null && !gDesc.startsWith(L10N_PREFIX)) {
+				String i18nKey = L10N_PREFIX + "group." + h.getName() + "." + g.getName() + ".description";
+				properties.setProperty(i18nKey, gDesc);
+				g.setDescription(i18nKey);
+			}
+		}
+	}
+
+	private void processFeature(Feature f) {
+		String name = f.getName();
+		
+		if(!f.getTitle().startsWith(L10N_PREFIX)) {
+			String i18nKey = L10N_PREFIX + "feature." + name + ".title";
+			properties.setProperty(i18nKey, f.getTitle());
+			f.setTitle(i18nKey);
+		}
+		
+		if(StringUtils.stripToNull(f.getDescription()) != null 
+				&& !f.getDescription().startsWith(L10N_PREFIX)) {
+			
+			String i18nKey = L10N_PREFIX + "feature." + name + ".description";
+			properties.setProperty(i18nKey, f.getDescription());
+			f.setDescription(i18nKey);
+			
+		}
+		
+		processTags(f, name);
+		processMoreTags(f, name);
+	}
+
+	private void processTags(Feature f, String name) {
+		
+		for(Tags sinonym : f.getTags()) {
+			for(Tag tag : sinonym.getTag()) {
+				processTag(name, tag, "tags");
+			}
+		}
+	}
+
+	private void processMoreTags(Feature f, String name) {
+		
+		List<MoreTags> moreTags = f.getMoreTags();
+		
+		for(MoreTags mt : moreTags) {
+			for(Tag tag : mt.getTag()) {
+				processTag(name, tag, "more_tags");
+			}
+		}
+	}
+
+	private void processTag(String name, Tag tag, String tagI18nKey) {
+		KeyType key = tag.getKey();
+		
+		String title = tag.getTitle();
+		if(!title.startsWith(L10N_PREFIX)) {
+			String i18nKey = L10N_PREFIX + "feature." + name + "." + tagI18nKey + "." + key.getValue() + ".title";
+			properties.setProperty(i18nKey, title);
+			
+			tag.setTitle(i18nKey);
+		}
+		
+		String description = tag.getDescription();
+		if(description!= null && !description.startsWith(L10N_PREFIX)) {
+			String i18nKey = L10N_PREFIX + "feature." + name + "." + tagI18nKey + "."+ key.getValue() + ".description";
+			properties.setProperty(i18nKey, description);
+			
+			tag.setTitle(i18nKey);
+		}
+		
+		for(Val val : tag.getVal()) {
+			String valueKey = val.getValue();
+			String valTitle = val.getTitle();
+			String valDesc = val.getDescription();
+			
+			if(!valTitle.startsWith(L10N_PREFIX)) {
+				String i18nKey = L10N_PREFIX + "feature." + name + "." + tagI18nKey + "."+ key.getValue() + "." + valueKey + ".title";
+				properties.setProperty(i18nKey, valTitle);
+				val.setTitle(i18nKey);
+			}
+
+			if(valDesc != null && !valDesc.startsWith(L10N_PREFIX)) {
+				String i18nKey = L10N_PREFIX + "feature." + name + "." + tagI18nKey + "."+ key.getValue() + "." + valueKey + ".description";
+				properties.setProperty(i18nKey, valDesc);
+				val.setDescription(i18nKey);
+			}
 		}
 	}
 
